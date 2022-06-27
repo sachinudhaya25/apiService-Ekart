eff4f1a7d3900522cb809d551827bc39dbb49a75
sachin <nudhayaraj99@gmail.com>
2022-06-27 17:18:38 +0530

    first commit

---
+++ src/main/java/com/eKart/api/ApiServiceEkartApplication.java
@@ -0,0 +1,13 @@
+package com.eKart.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceEkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServiceEkartApplication.class, args);
+	}
+
+}
