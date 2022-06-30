fc3b0b36b5f79675f89ae89ab09615f508caf45f
sachin <nudhayaraj99@gmail.com>
2022-06-30 10:46:38 +0530

    Added swagger and auth service

--- src/main/java/com/eKart/api/ApiServiceEkartApplication.java
+++ src/main/java/com/eKart/api/ApiServiceEkartApplication.java
@@ -2,12 +2,23 @@ package com.eKart.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ApiServiceEkartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiServiceEkartApplication.class, args);
 	}
-
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
